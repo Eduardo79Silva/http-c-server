ec0966a948ee3985f8ac947b738f7b571ba7d4cb
Eduardo Silva <up202005283@up.pt>
2024-02-13 10:37:39 +0000

    feat: rewriting main commit

--- main.c
+++ main.c
@@ -1,5 +1,6 @@
 
 
+#include <errno.h>
 #include <fcntl.h>
 #include <netinet/in.h>
 #include <stdio.h>
@@ -10,117 +11,15 @@
 #include <unistd.h>
 
 #include "http.h"
+// #include "Routes.h"
+// #include "Response.h"
 
 int main() {
   // initiate HTTP_Server
   Server http_server;
-  init_server(&http_server, 6969);
+  init_server(&http_server, 8080);
 
   int client_socket;
-
-  // registering Routes#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <sys/socket.h>
-#include <sys/types.h>
-#include <netinet/in.h>
-#include <fcntl.h>
-#include <unistd.h>
-
-#include "http.h"
-#include "Routes.h"
-#include "Response.h"
-
-int main() {
-	// initiate HTTP_Server
-	HTTP_Server http_server;
-	init_server(&http_server, 8080);
-
-	int client_socket;
-
-	// registering Routes
-	struct Route * route = initRoute("/", "index.html");
-	addRoute(route, "/about", "about.html");
-
-
-	printf("\n====================================\n");
-	printf("=========ALL VAILABLE ROUTES========\n");
-	// display all available routes
-	inorder(route);
-
-	while (1) {
-		char client_msg[4096] = "";
-
-		client_socket = accept(http_server.socket, NULL, NULL);
-
-		read(client_socket, client_msg, 4095);
-		printf("%s\n", client_msg);
-
-		// parsing client socket header to get HTTP method, route
-		char *method = "";
-		char *urlRoute = "";
-
-		char *client_http_header = strtok(client_msg, "\n");
-
-		printf("\n\n%s\n\n", client_http_header);
-
-		char *header_token = strtok(client_http_header, " ");
-
-		int header_parse_counter = 0;
-
-		while (header_token != NULL) {
-
-			switch (header_parse_counter) {
-				case 0:
-					method = header_token;
-				case 1:
-					urlRoute = header_token;
-			}
-			header_token = strtok(NULL, " ");
-			header_parse_counter++;
-		}
-
-		printf("The method is %s\n", method);
-		printf("The route is %s\n", urlRoute);
-
-
-		char template[100] = "";
-
-		if (strstr(urlRoute, "/static/") != NULL) {
-			//strcat(template, urlRoute+1);
-			strcat(template, "static/index.css");
-		}else {
-			struct Route * destination = search(route, urlRoute);
-			strcat(template, "templates/");
-
-			if (destination == NULL) {
-				strcat(template, "404.html");
-			}else {
-				strcat(template, destination->value);
-			}
-		}
-
-		char * response_data = render_static_file(template);
-
-		char http_header[4096] = "HTTP/1.1 200 OK\r\n\r\n";
-
-		strcat(http_header, response_data);
-		strcat(http_header, "\r\n\r\n");
-
-
-		send(client_socket, http_header, sizeof(http_header), 0);
-		close(client_socket);
-		free(response_data);
-	}
-	return 0;
-}
-  struct Route *route = initRoute("/", "index.html");
-  addRoute(route, "/about", "about.html");
-
-  printf("\n====================================\n");
-  printf("=========ALL VAILABLE ROUTES========\n");
-  // display all available routes
-  inorder(route);
 
   while (1) {
     char client_msg[4096] = "";
@@ -129,51 +28,11 @@ int main() {
 
     read(client_socket, client_msg, 4095);
     printf("%s\n", client_msg);
+    FILE *html_data;
+    html_data = fopen("index.html", "r");
 
-    // parsing client socket header to get HTTP method, route
-    char *method = "";
-    char *urlRoute = "";
-
-    char *client_http_header = strtok(client_msg, "\n");
-
-    printf("\n\n%s\n\n", client_http_header);
-
-    char *header_token = strtok(client_http_header, " ");
-
-    int header_parse_counter = 0;
-
-    while (header_token != NULL) {
-
-      switch (header_parse_counter) {
-      case 0:
-        method = header_token;
-      case 1:
-        urlRoute = header_token;
-      }
-      header_token = strtok(NULL, " ");
-      header_parse_counter++;
-    }
-
-    printf("The method is %s\n", method);
-    printf("The route is %s\n", urlRoute);
-
-    char template[100] = "";
-
-    if (strstr(urlRoute, "/static/") != NULL) {
-      // strcat(template, urlRoute+1);
-      strcat(template, "static/index.css");
-    } else {
-      struct Route *destination = search(route, urlRoute);
-      strcat(template, "templates/");
-
-      if (destination == NULL) {
-        strcat(template, "404.html");
-      } else {
-        strcat(template, destination->value);
-      }
-    }
-
-    char *response_data = render_static_file(template);
+    char response_data[1024];
+    fgets(response_data, 1024, html_data);
 
     char http_header[4096] = "HTTP/1.1 200 OK\r\n\r\n";
 
@@ -182,7 +41,6 @@ int main() {
 
     send(client_socket, http_header, sizeof(http_header), 0);
     close(client_socket);
-    free(response_data);
   }
   return 0;
 }
